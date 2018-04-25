b4a4de776f9a7bcc583449c687f30a179d3a1900
Alexander Velasquez <rastalextremo@gmail.com>
2018-04-25 00:13:09 -0500

    Reto: Como modificar el script de inicializacion la base de datos

--- setup.js
+++ setup.js
@@ -7,18 +7,29 @@ const chalk = require('chalk')
 const prompt = inquirer.createPromptModule()
 
 async function setup () {
-  const answer = await prompt([
-    {
-      type: 'confirm',
-      name: 'confirmacion',
-      message: 'Esto va a destruir la base de datos, está seguro ?'
+  let flag = false
+  
+  process.argv.forEach(e => {
+    if (e === '--Y') {
+      flag = true
     }
-  ])
+  })
 
-  if (!answer.confirmacion) {
-    return console.log('No hacemos nada entonces...')
+  if(!flag) {
+
+    const answer = await prompt([
+      {
+        type: 'confirm',
+        name: 'confirmacion',
+        message: 'Esto va a destruir la base de datos, está seguro ?'
+      }
+    ])
+  
+    if (!answer.confirmacion) {
+      return console.log('No hacemos nada entonces...')
+    }
   }
-
+  
   const config = {
     database: process.env.DB_NAME || 'platziverse',
     username: process.env.DB_USERNAME || 'platzi',
