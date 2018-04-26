3816a5c51c564b5efa8a9e395159f53e46c831c9
Alexander Velasquez <rastalextremo@gmail.com>
2018-04-26 17:09:26 -0500

    Como almacenar la informacion del agente y reportar el agente conectado

--- platziverse-mqtt/server.js
+++ platziverse-mqtt/server.js
@@ -1,3 +1,5 @@
+import { SSL_OP_NETSCAPE_DEMO_CIPHER_CHANGE_BUG } from 'constants';
+
 'use strict'
 
 const debug = require('debug')('platziverse:mqtt')
@@ -5,6 +7,8 @@ const mosca = require('mosca')
 const redis = require('redis')
 const chalk = require('chalk')
 const db = require('platziverse-db')
+const conf = require('../platziverse-db/config-db.js')
+const { parsePayload } = require('./utils.js')
 
 const backend = {
   type: "redis",
@@ -17,16 +21,9 @@ const settings = {
   backend
 }
 
-const config = {
-  database: process.env.DB_NAME || 'platziverse',
-  username: process.env.DB_USERNAME || 'platzi',
-  password: process.env.DB_PASS || 'platzi',
-  host: process.env.DB_HOST || 'localhost',
-  dialect: 'postgres',
-  logging: s => debug(s),
-}
-
+const config = conf(false)
 const server = new mosca.Server(settings)
+const clients = new Map()
 
 let Agent, Metric
 
@@ -41,15 +38,56 @@ server.on('ready', async () => {
 
 server.on('clientConnected', client => {
   debug(`Cliente Conectado: ${client.id}`) //id generado automaticamente
+  clients.set(client.id, null)
 })
 
 server.on('clientDisconnected', client => {
   debug(`Cliente Desconectado: ${client.id}`) //id generado automaticamente
 })
 
-server.on('published', (packet, client) => {
+server.on('published', async (packet, client) => {
   debug(`Recibido: ${packet.topic}`)
-  debug(`Payload: ${packet.payload}`)
+
+  switch (package.topic) {
+    case 'agent/connected':
+    case 'agent/disconnected':
+      debug(`Payload: ${packet.payload}`)
+      break
+    case 'agent/message':
+        debug(`Payload ${packet.payload}`)
+        const payload = parsePayload(packet.payload)
+
+      if(payload) {
+        payload.agent.connected = true
+
+        let agent
+        try {
+          agent = await Agent.createOrUpdate(payload.agent)
+        } catch (e) {
+          return handleError(e)
+        }
+
+        debug(`Agent ${agent.uuid} guardado`)
+
+        //Notificar que el agente esta conectado
+        if (!clients.get(client.id)) {
+          clients.set(client.id, agent)
+          server.publish({
+            topic: 'agent/connected',
+            payload: JSON.stringify({
+              agent: {
+                uuid:agent,
+                name: agent.name,
+                hostname: agent.hostname,
+                pid: agent.pid,
+                connected: agent.connected
+              }
+            })
+          })
+        }
+      }
+      break
+  }
 })
 
 server.on('error', handleFatalError)
@@ -58,6 +96,11 @@ function handleFatalError () {
   console.error(`${chalk.red('Error Fatal')} ${err.message}`)
   console.error(err.stack)
   process.exit(1)
+}
+
+function handleError () {
+  console.error(`${chalk.red('Error')} ${err.message}`)
+  console.error(err.stack)
 }
 
 process.on('uncaughtException', handleFatalError)
